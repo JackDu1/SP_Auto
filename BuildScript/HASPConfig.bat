f9edd281f1a6a64ab100e25f4cbff2ca9b7b478a
DarrennJiang <Darrenn.Jiang@software.dell.com>
2017-09-25 15:30:57 +0800

    update SP_Auto FILE

--- BuildScript/HASPConfig.bat
+++ BuildScript/HASPConfig.bat
@@ -8,7 +8,7 @@ sc stop hasplms
 	sc query hasplms |findstr "STOPPED" > nul || goto :waitstop
 
 echo [REMOTE] > "%HASP%\hasplm.ini"
-echo serveraddr=12.106.87.9 >> "%HASP%\hasplm.ini"
+echo serveraddr=10.1.145.11 >> "%HASP%\hasplm.ini"
 echo aggressive=1 >> "%HASP%\hasplm.ini"
 echo broadcastsearch=0 >> "%HASP%\hasplm.ini"
 
